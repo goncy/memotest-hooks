a960d092de2e158e41228fe1769f4e1cb603bc67
Gonzalo Pozzo <gonzalo.pozzo4@gmail.com>
2019-12-20 14:11:37 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import "@fortawesome/fontawesome-free/css/all.css"
-
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
